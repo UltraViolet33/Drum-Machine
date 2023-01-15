d93dd15e12d14b6348c011f3b3099624a5df2711
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-15 11:05:49 +0100

    add AudioElement

---
+++ src/components/AudioElement.js
@@ -0,0 +1,7 @@
+export const AudioElement = ({ source }) => {
+  return (
+    <div className="drum-pad">
+      <audio src={source}></audio>Q
+    </div>
+  );
+};
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,9 +1,18 @@
 import "./DrumPad.css";
-
+import sound from "../assets/Heater-1.mp3";
 
 export const DrumPad = () => {
+  const audioElement = new Audio(sound);
+
+  const playSound = () => {
+    audioElement.play();
+  };
+
   return (
     <div id="drum-pad-container">
+      <div className="drum-pad" onClick={playSound}>
+        <audio src={sound}></audio>Q
+      </div>
       <div className="drum-pad">Q</div>
       <div className="drum-pad">Q</div>
       <div className="drum-pad">Q</div>
@@ -12,8 +21,6 @@ export const DrumPad = () => {
       <div className="drum-pad">Q</div>
       <div className="drum-pad">Q</div>
       <div className="drum-pad">Q</div>
-      <div className="drum-pad">Q</div>
-      
     </div>
   );
 };

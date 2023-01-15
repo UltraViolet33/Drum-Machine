8722f66f1b0441066c4e47c7063d807b7c6dea68
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-15 11:08:52 +0100

    add GlobalAudio

---
+++ src/assets/GlobalAudio.js
@@ -0,0 +1,13 @@
+import audio from "./Heater-1.mp3";
+
+export const GlobalAudio = {
+  audio1: audio,
+  audio2: audio,
+  audio3: audio,
+  audio4: audio,
+  audio5: audio,
+  audio6: audio,
+  audio7: audio,
+  audio8: audio,
+  audio9: audio,
+};

859e705360d784a17a43e50f1baec321743bb249
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-15 15:31:26 +0100

    pass freecodecamp test

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,8 +1,6 @@
 import "./Control.css";
 
 export const Control = ({ displayText }) => {
-
-  console.log(displayText);
   return (
     <div id="control-container">
       <p id="display">{displayText}</p>
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -4,30 +4,29 @@ import { AudioElement } from "./AudioElement";
 import { useEffect } from "react";
 
 export const DrumPad = ({ setDisplayText }) => {
-
   const playSound = audioToPlay => {
-    const audioElement = new Audio(audioToPlay.source);
+    const audioElement = document.getElementById(audioToPlay.key);
     setDisplayText(audioToPlay.id);
     audioElement.play();
   };
 
-  const playSoundFromKeyboard = key => {
+  const playSoundFromKeyboard = event => {
     const audioToPlay = GlobalAudio.find(
-      audio => audio.key === key.toUpperCase()
+      audio => audio.key === event.key.toUpperCase()
     );
 
     if (audioToPlay) {
+      console.log(audioToPlay);
       playSound(audioToPlay);
     }
   };
 
   useEffect(() => {
-    const keyDownHandler = event => {
-      document.addEventListener("keydown", keyDownHandler);
-      playSoundFromKeyboard(event.key);
+    document.addEventListener("keydown", playSoundFromKeyboard);
+    return () => {
+      document.removeEventListener("keydown", playSoundFromKeyboard);
     };
-    document.addEventListener("keydown", keyDownHandler);
-  }, []);
+  });
 
   return (
     <div id="drum-pad-container">

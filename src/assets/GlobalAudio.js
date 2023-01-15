a40dab3861456f6c03fd1c1b15550acd170b7e2c
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-15 11:41:56 +0100

    play sound from keyboard

--- src/assets/GlobalAudio.js
+++ src/assets/GlobalAudio.js
@@ -1,13 +1,21 @@
-import audio from "./Heater-1.mp3";
+import audio1 from "./Heater-1.mp3";
+import audio2 from "./Heater-2.mp3";
+import audio3 from "./Heater-3.mp3";
+import audio4 from "./Heater-4_1.mp3";
+import audio5 from "./Cev_H2.mp3";
+import audio6 from "./Dsc_Oh.mp3";
+import audio7 from "./Kick_n_Hat.mp3";
+import audio8 from "./RP4_KICK_1.mp3";
+import audio9 from "./Heater-6.mp3";
 
 export const GlobalAudio = [
-  { key: "A", source: audio },
-  { key: "Z", source: audio },
-  { key: "E", source: audio },
-  { key: "Q", source: audio },
-  { key: "S", source: audio },
-  { key: "D", source: audio },
-  { key: "W", source: audio },
-  { key: "X", source: audio },
-  { key: "C", source: audio },
+  { key: "A", source: audio1 },
+  { key: "Z", source: audio2 },
+  { key: "E", source: audio3 },
+  { key: "Q", source: audio4 },
+  { key: "S", source: audio5 },
+  { key: "D", source: audio6 },
+  { key: "W", source: audio7 },
+  { key: "X", source: audio8 },
+  { key: "C", source: audio9 },
 ];
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -2,6 +2,7 @@ import "./DrumPad.css";
 import sound from "../assets/Heater-1.mp3";
 import { GlobalAudio } from "../assets/GlobalAudio";
 import { AudioElement } from "./AudioElement";
+import { useEffect } from "react";
 
 export const DrumPad = () => {
   const audioElement = new Audio(sound);
@@ -9,6 +10,24 @@ export const DrumPad = () => {
   const playSound = () => {
     audioElement.play();
   };
+
+  const playSoundFromKeyboard = key => {
+    console.log(key);
+    GlobalAudio.forEach(audio => {
+      if (audio.key === key.toUpperCase()) {
+        const audioElement = new Audio(audio.source);
+        audioElement.play();
+      }
+    });
+  };
+
+  useEffect(() => {
+    const keyDownHandler = event => {
+      document.addEventListener("keydown", keyDownHandler);
+      playSoundFromKeyboard(event.key);
+    };
+    document.addEventListener("keydown", keyDownHandler);
+  });
 
   return (
     <div id="drum-pad-container">
